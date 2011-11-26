2c651800df88f8b2ef57e34930608f8e67487e7c
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-11-26 15:42:24 +0100

    initial commit

---
+++ extension.driver.php
@@ -0,0 +1,17 @@
+<?php
+	
+	Class extension_email_statistics extends Extension{
+
+		public function about(){
+			return array(
+				'name' => 'Email Statistics',
+				'version' => '0.1',
+				'release-date' => '2011-11-26',
+				'author' => array(
+					'name' => 'Huib Keemink',
+					'website' => 'http://www.creativedutchmen.com',
+					'email' => 'huib.keemink@creativedutchmen.com'
+				)
+			);
+		}
+	}
